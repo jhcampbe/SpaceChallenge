74032cf4ce319635dd8dcd58b96e5125b30ecd43
Jordan Campbell <jhcampbe3@gmail.com>
2018-09-14 05:24:55 -0700

    Add loadU1 and loadU2 functions to Simulation class

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Simulation {
 
-    private void loadItems(){
+    public ArrayList<Item> loadItems(){
         File file = new File("phase-1.txt");
         Scanner scanner = null;
         try{
@@ -13,11 +13,48 @@ public class Simulation {
 
         }
 
+        ArrayList<Item> items = new ArrayList<>();
         while (scanner.hasNextLine()){
-//            String[] data = scanner.nextLine().split("=");
-//            Item item = new Item();
-
+            String[] data = scanner.nextLine().split("=");
+            String name = data[0];
+            int weight = Integer.parseInt(data[1]);
+            Item item = new Item(name, weight);
+            items.add(item);
         }
 
+        return items;
+
     }
+
+    public ArrayList<U1> loadU1(ArrayList<Item> items){
+        int i = 0;
+        ArrayList<U1> U1rockets = new ArrayList<>();
+        while (i < items.size()){
+            U1 u1 = new U1();
+            while(i < items.size() && u1.canCarry(items.get(i))){
+                u1.carry(items.get(i));
+                i++;
+            }
+            U1rockets.add(u1);
+        }
+
+        return U1rockets;
+    }
+
+    public ArrayList<U2> loadU2(ArrayList<Item> items){
+        int i = 0;
+        ArrayList<U2> U2rockets = new ArrayList<>();
+        while (i < items.size()){
+            U2 u2 = new U2();
+            while(i < items.size() && u2.canCarry(items.get(i))){
+                u2.carry(items.get(i));
+                i++;
+            }
+            U2rockets.add(u2);
+        }
+
+        return U2rockets;
+    }
+
+
 }
