92f614e265b1373c1312d6013c950ec75a202344
Jordan Campbell <jhcampbe3@gmail.com>
2018-09-14 03:53:56 -0700

    Create U2 class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        U1 test = new U1();
-        System.out.println(test.cost + ", " + test.cargoCarried + ", " + test.cargoLimit);
 
     }
 }
