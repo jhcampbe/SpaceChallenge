f11fb090e395cee1f975a9de4f161a649a6a12dd
Jordan Campbell <jhcampbe3@gmail.com>
2018-09-14 04:33:00 -0700

    Add loadItems function to Simulation class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,20 @@
+import java.io.*;
+import java.util.*;
+
 public class Simulation {
+
+    private void loadItems(){
+        File file = new File("phase-1.txt");
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException exception){
+            System.out.println("File not found.");
+
+        }
+
+        while (scanner.hasNextLine()){
+        }
+
+    }
 }
